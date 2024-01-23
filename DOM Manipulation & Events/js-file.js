50591958990789236135720eacc8beb90e68c550
vjhwong <victor.ju.hin.wong@gmail.com>
2024-01-23 08:50:26 +0100

    JS course

--- DOM Manipulation & Events/js-file.js
+++ DOM Manipulation & Events/js-file.js
@@ -1,37 +1,10 @@
-const container = document.querySelector("#container");
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll("button");
 
-const content = document.createElement("div");
-content.classList.add("content");
-content.textContent = "This is the glorious text-content!";
-
-container.appendChild(content);
-
-const redParagraph = document.createElement("p");
-redParagraph.textContent = "Hey I'm red!";
-redParagraph.style.color = "red";
-
-container.appendChild(redParagraph);
-
-const blueHeader = document.createElement("h3");
-blueHeader.textContent = "I'm a blue h3!";
-blueHeader.style.color = "blue";
-
-container.appendChild(blueHeader);
-
-const div = document.createElement("div");
-div.setAttribute(
-  "style",
-  "border-style: solid; background-color: pink; border-color: black;"
-);
-
-const divHeader = document.createElement("h1");
-divHeader.textContent = "I'm in a div!";
-
-div.appendChild(divHeader);
-
-const divParagraph = document.createElement("p");
-divParagraph.textContent = "ME TOO!";
-
-div.appendChild(divParagraph);
-
-container.appendChild(div);
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+  // and for each one we add a 'click' listener
+  button.addEventListener("click", () => {
+    alert(button.id);
+  });
+});
